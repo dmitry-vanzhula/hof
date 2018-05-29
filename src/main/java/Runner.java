05601b2a5fa98e5b70be34348e8b3bbc7bb75d6d
dvanzhula <dvanzhula@gmail.com>
2018-05-29 12:08:55 +0300

    updated lines for games

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,10 +1,9 @@
 public class Runner extends JsHandler {
 
     public static void main(String[] args) throws Exception {
-       // String path = "E:\\myapp\\src\\main\\resources\\res.json";
-        String path = "E:\\myapp\\src\\main\\resources\\gummy.json";
-        String[] expLines = Lines.fillWithLinesIndependent4x5x40();
-        System.out.println(CompareObjects.isEqualsLines(expLines, convertLinesFromJsArrayToJavaArray(40, getJsArrayLines
+        String path = "src\\main\\resources\\the_hattress.json";
+        String[] expLines = Lines.fillWithLinesIndependent5x4x50V2();
+        System.out.println(CompareObjects.isEqualsLines(expLines, convertLinesFromJsArrayToJavaArray(50, getJsArrayLines
                         (convertJsStringToJsObject(FileReader.convertFromFileToString(FileReader.readFile(path)))))));
 
     }
